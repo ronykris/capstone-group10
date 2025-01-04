1268457669208e533c89796f3b20cd8e8bce6554
ronykris <rony.kris@gmail.com>
2025-01-04 12:36:50 +0530

    containerise

--- detection/app/main.py
+++ detection/app/main.py
@@ -4,10 +4,11 @@ import io, os
 
 app = FastAPI()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(BASE_DIR, "models", "trained_model_20241222-221022.pt")
-detector = FoodDetector(model_path)
-#detector = FoodDetector("./detection/app/models/trained_model_20241222-221022.pt")
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#print(f"{BASE_DIR}")
+#model_path = os.path.join(BASE_DIR, "models", "trained_model_20241222-221022.pt")
+#detector = FoodDetector(model_path)
+detector = FoodDetector("models/trained_model_20241222-221022.pt")
 
 @app.post("/detect")
 async def detect_food(file: UploadFile = File(...)):
